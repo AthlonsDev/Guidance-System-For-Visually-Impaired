8b27cb9d8d5d44ccc7c381146f83e5693cfb3aa8
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2023-12-21 22:33:45 +0000

    BLE Connection established

--- Microcontroller/BLE_receiver.py
+++ Microcontroller/BLE_receiver.py
@@ -1,42 +1,51 @@
-import pygatt # pygatt is a Python 2.x and 3.x module that allows you to communicate with Bluetooth LE devices.
-import binascii # This module contains a number of methods to convert between binary and various ASCII-encoded binary representations.
-import time
+# from bleson import get_provider, Observer, UUID16
+# from time import sleep
 
-pico_uuid = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
-mac = "d8:3a:dd:5c:49:ab"
+# def on_advertisement(advertisement):
+#     print(advertisement)
+#     mfg_data = advertisement.mfg_data
+#     address = advertisement.address
 
-#define the MAC address and UUID of the target deviec and characteristic
-MAC = mac # MAC address is a unique identifier assigned to a Bluetooth device during the manufacturing process.
-UUID = pico_uuid # UUID is a 128-bit number used to uniquely identify information.
+#     if mfg_data is not None:
+#         print("-" * 50, "Advert", "-"*50)
+#         print(f"""
+#           Device Name: {advertisement._name}
+#           Address: {advertisement.address_type}: {advertisement.address}
+#           Raw:{advertisement.raw_data} \t Raw Hex:{advertisement.raw_data.hex()}
+#           Mgf Date: {advertisement.mfg_data}        
+#         """)
 
-# define exadecimal command to send
-command_str = "toggle" # toggle is a command to turn on/off the LED
-hex_command = binascii.hexlify(command_str.encode()) # encode() method returns an encoded version of the string.
-byte_command = binascii.unhexlify(hex_command) # unhexlify() method returns the binary data represented by the hexadecimal string.
+# adapter = get_provider().get_adapter()
 
-#create a Pygatt BLE adapter
-adapter = pygatt.GATTToolBackend() # GATTToolBackend is a Python interface for the gatttool (a deprecated tool from BlueZ) using the BGAPI backend from pygattlib.
+# observer = Observer(adapter)
+# observer.on_advertising_data = on_advertisement
 
-try:
-    #start the adapter
-    adapter.start()
-    print('Start')
-    time.sleep(10)
+# observer.start()
+# while True:
+#     sleep(10)
 
-    #connect to the device
-    device = adapter.connect(MAC) # connect() method connects to the device with the specified MAC address.
-    print('Connected')
-    time.sleep(10)
+# observer.stop()
 
-    #send command
-    device.char_write(UUID, byte_command) # char_write() method writes the value of the specified characteristic.
+import asyncio
+from bleak import BleakScanner, BleakClient
 
-    #receive response
-    response = device.char_read(UUID) # char_read() method reads the value of the specified characteristic.
-    print(response)
+async def run():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print(d)
 
-finally:
-    #stop the adapter session
-    adapter.stop()
-    print('Stop')
-    time.sleep(10)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
+
+#connect to device with address
+address = "D8:3A:DD:5C:49:AC:"
+MODEL_NBR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" #this is the UUID for the characteristic that we want to read, 
+
+async def main(address):
+    async with BleakClient(address) as client:
+        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
+        print("Model Number: {0}".format("".join(map(chr, model_number))))
+
+asyncio.run(main(address))
