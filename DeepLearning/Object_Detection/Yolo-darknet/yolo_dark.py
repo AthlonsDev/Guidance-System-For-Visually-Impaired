9b1719505b0c3923332101ffe91566676c5231c5
athlonsDev <athlons91@gmail.com>
2024-01-19 15:58:29 +0000

    Darknet and OpenCV implementation

--- DeepLearning/Object_Detection/Yolo-darknet/yolo_dark.py
+++ DeepLearning/Object_Detection/Yolo-darknet/yolo_dark.py
@@ -1,2 +1,50 @@
-from matplotlib import image, patches, pyplot as plt
-from darknetpy.detector import Detector
+import cv2
+import time
+import threading as th
+from Speech import Speech1
+
+CONFIDENCE_THRESHOLD = 0.2
+NMS_THRESHOLD = 0.4
+COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+
+
+class_names = []
+with open("/home/athlons/Documents/Final_Project/DeepLearning/Object_Detection/Yolo-darknet/coco-classes.txt", "r") as f:
+    class_names = [cname.strip() for cname in f.readlines()]
+
+vc = cv2.VideoCapture("/home/athlons/Documents/Final_Project/realtime_obj_det/test_videos/video-2.mp4")
+
+net = cv2.dnn.readNet("/home/athlons/Documents/Final_Project/DeepLearning/Object_Detection/Yolo-darknet/yolov4-tiny.weights", "/home/athlons/Documents/Final_Project/DeepLearning/Object_Detection/Yolo-darknet/darknet/cfg/yolov4-tiny.cfg")
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
+
+while cv2.waitKey(1) < 1:
+    (grabbed, frame) = vc.read()
+    if not grabbed:
+        exit()
+
+    start = time.time()
+    classes, scores, boxes = model.detect(frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+    end = time.time()
+
+    start_drawing = time.time()
+    for (classid, score, box) in zip(classes, scores, boxes):
+        color = COLORS[int(classid) % len(COLORS)]
+        label = "%s : %f" % (class_names[classid], score)
+        cv2.rectangle(frame, box, color, 2)
+        cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        print(class_names[classid])
+        speech = Speech()
+        t = th.Thread(target=speech.say, args=(class_names[classid],))
+        t.start()
+        t.join()
+        time.sleep(3)
+    end_drawing = time.time()
+    
+    fps_label = "FPS: %.2f (excluding drawing time of %.2fms)" % (1 / (end - start), (end_drawing - start_drawing) * 1000)
+    cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+    cv2.imshow("detections", frame)
+    
