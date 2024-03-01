48d1d33810780bbd68da1cae749d514e0bd14736
athlonsdev <athlons91@gmail.com>
2024-03-01 16:03:29 +0000

    move receiver directory speech fixes

--- DeepLearning/Object_Detection/Yolo-darknet/Speech.py
+++ DeepLearning/Object_Detection/Yolo-darknet/Speech.py
@@ -1,18 +1,30 @@
 import pyttsx3
-# from gtts import gTTS
+from gtts import gTTS
 from io import BytesIO
 
+engine = pyttsx3.init()
+engine.setProperty('rate', 150) # setting up new voice rate, the speed of the voice
+engine.setProperty('volume', 1.0) # setting up volume level  between 0 and 1
+voices = engine.getProperty('voices') #getting details of current voice, get the voice property 
+engine.setProperty('voice', voices[11].id)
 
-# engine = pyttsx3.init()
-# engine.runAndWait() # this is needed to initialize the engine and wait for it to finish initializing
+def speak(text):
+
+    engine.say(text)
+    
+    if engine._inLoop:
+        engine.endLoop()
+    else:
+        engine.runAndWait()
 
 class Speech():
     def __init__(self):
         self.engine = pyttsx3.init()
-        self.engine.setProperty('rate', 150) # setting up new voice rate, the speed of the voice
+        self.engine.setProperty('rate', 100) # setting up new voice rate, the speed of the voice
         self.engine.setProperty('volume', 1.0) # setting up volume level  between 0 and 1
         voices = self.engine.getProperty('voices') #getting details of current voice, get the voice property 
-        self.engine.setProperty('English', voices[1].id)
+        self.engine.setProperty('voice', 'mb-en1')
+
     def say(self, text):
         self.engine.say(text)
         self.engine.runAndWait()
@@ -23,15 +35,3 @@ class TextToSpeech():
         fp = BytesIO()
         tts.write_to_fp(fp)
 
-
-def main():
-    sp = Speech()
-    # sp.say("Hello world!")
-    # engine.runAndWait()
-    tts = TextToSpeech()
-    tts.speak("Hello world!")
-    
-
-
-if __name__ == "__main__":
-    main()
